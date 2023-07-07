39e5e686dc55d49a125b05a19419064507d8fd1f
butvinm <butvin.mihail@yandex.ru>
2023-07-07 04:34:38 +0300

    Fix incorrect JWT secret

--- backend/scripts/space-dev.bat
+++ backend/scripts/space-dev.bat
@@ -1,5 +1,5 @@
 poetry export -f requirements.txt -o requirements.txt --without-hashes
-SET JWT_SECRET_KEY="secret"
+SET JWT_SECRET_KEY=09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7
 SET ACCESS_TOKEN_EXPIRE_MINUTES=30
 cd ..
 space dev backend
