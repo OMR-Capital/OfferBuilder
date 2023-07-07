97f23c668e4ebe41499181f72bed8d1bc82f660a
butvinm <butvin.mihail@yandex.ru>
2023-07-07 17:47:12 +0300

    Add exceptions module

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,13 +1,13 @@
 """Authentication API."""
 
-from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.api.schemes.auth import Token
 from app.core.auth import authenticate_user, create_user_access_token
+from app.exceptions import Unauthorized
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -22,18 +22,15 @@ async def login(
         form_data (Annotated[OAuth2PasswordRequestForm, Depends): Auth data.
 
     Raises:
-        HTTPException: Return 404 if user credentials are invalid.
+        Unauthorized: If user is not authorized or token is invalid.
 
     Returns:
         Token: JWT access token.
     """
     user = await authenticate_user(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail='Incorrect username or password',
-            headers={'WWW-Authenticate': 'Bearer'},
-        )
+        raise Unauthorized()
 
     access_token = create_user_access_token(uid=user.uid)
-    return Token(access_token=access_token, token_type='bearer')
+    # `bearer` is a type of access token
+    return Token(access_token=access_token, token_type='bearer')  # noqa: S106
---
+++ backend/app/exceptions.py
@@ -0,0 +1,34 @@
+"""Extensions."""
+
+
+from http import HTTPStatus
+
+from fastapi import HTTPException
+
+
+class Unauthorized(HTTPException):
+    """Raised when the user is not authorized or the token is invalid."""
+
+    def __init__(self) -> None:
+        """Initialize the exception."""
+        self.status_code = HTTPStatus.UNAUTHORIZED
+        self.detail = 'Incorrect username or password'
+        self.headers = {'WWW-Authenticate': 'Bearer'}
+
+
+class AdminRightsRequired(HTTPException):
+    """Raised when the user is not authorized to access a resource."""
+
+    def __init__(self) -> None:
+        """Initialize the exception."""
+        self.status_code = HTTPStatus.FORBIDDEN
+        self.detail = 'Admin rights are required.'
+
+
+class UserNotFound(HTTPException):
+    """Raised when the user is not found."""
+
+    def __init__(self) -> None:
+        """Initialize the exception."""
+        self.status_code = HTTPStatus.NOT_FOUND
+        self.detail = 'User not found'
