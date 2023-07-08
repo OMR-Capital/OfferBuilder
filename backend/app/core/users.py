7094b965ff3b5b2a64181e4ce58253c444ff8273
butvinm <butvin.mihail@yandex.ru>
2023-07-08 06:25:26 +0300

    Restruct users getting methods and deps errors
    Remove HTTP errors from core.users, raise them only from deps

--- backend/app/core/users.py
+++ backend/app/core/users.py
@@ -8,7 +8,6 @@ from jose import JWTError
 
 from app.core.auth import get_access_token_payload
 from app.db.user import UserInDB
-from app.api.exceptions import AdminRightsRequired, Unauthorized
 from app.models.user import User, UserRole
 
 
@@ -21,47 +20,45 @@ def generate_uid() -> str:
     return ''.join(choice(ascii_letters) for _ in range(8))
 
 
-async def get_authorized_user(token: str) -> User:
+async def get_authorized_user(token: str) -> Optional[User]:
     """Get current user from JWT token.
 
     Args:
         token (str): JWT token from oauth2 scheme.
 
     Raises:
-        Unauthorized: If token is invalid or user is not found.
+        JWTError: If token is invalid
 
     Returns:
-        User: User instance.
+        Optional[User]: User instance if found, None otherwise.
     """
     try:
         payload = get_access_token_payload(token)
-    except JWTError:
-        raise Unauthorized()
+    except JWTError as exc:
+        # Explicitly raise Unauthorized exception
+        raise exc
 
     uid: Optional[str] = payload.get('sub')
     if uid is None:
-        raise Unauthorized()
+        return None
 
     db_user = await UserInDB.get_or_none(uid)
     if db_user is None:
-        raise Unauthorized()
+        return None
 
     return User(**db_user.dict())
 
 
-async def verify_admin(user: User) -> User:
-    """Verify that user is admin.
+async def get_verified_admin(user: User) -> Optional[User]:
+    """Return user, verified to be admin.
 
     Args:
         user (User): Current user.
 
-    Raises:
-        AdminRightsRequired: If user is not admin.
-
     Returns:
-        User: Admin user.
+        Optional[User]: Admin user if it is admin, None otherwise.
     """
     if user.role != UserRole.admin:
-        raise AdminRightsRequired()
+        return None
 
     return user
--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -5,7 +5,9 @@ from typing import Annotated
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 
+from app.api.exceptions import AdminRightsRequired, Unauthorized
 from app.core import users
 from app.models.user import User
 
@@ -22,10 +24,21 @@ async def get_current_user(
     Args:
         token (str): JWT token from oauth2 scheme.
 
+    Raises:
+        Unauthorized: If token is invalid or user is not found.
+
     Returns:
         User: User instance.
     """
-    return await users.get_authorized_user(token)
+    try:
+        user = await users.get_authorized_user(token)
+    except JWTError:
+        raise Unauthorized()
+
+    if user is None:
+        raise Unauthorized()
+
+    return user
 
 
 async def get_admin(
@@ -38,7 +51,14 @@ async def get_admin(
     Args:
         user (User): Current user.
 
+    Raises:
+        AdminRightsRequired: If user is not admin.
+
     Returns:
         User: Admin user.
     """
-    return await users.verify_admin(user)
+    admin = await users.get_verified_admin(user)
+    if admin is None:
+        raise AdminRightsRequired()
+
+    return admin
