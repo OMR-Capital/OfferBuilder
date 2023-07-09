b5fc209a12cfe1b3c6d5e1c5d0bd1249e425f95e
butvinm <butvin.mihail@yandex.ru>
2023-07-10 02:02:53 +0300

    Add AGENTS_API_KEY env for agents api

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,3 +8,5 @@ from os import environ
 JWT_SECRET_KEY = environ['JWT_SECRET_KEY']
 
 ACCESS_TOKEN_EXPIRE_MINUTES = int(environ['ACCESS_TOKEN_EXPIRE_MINUTES'])
+
+AGENTS_API_KEY = environ['AGENTS_API_KEY']
