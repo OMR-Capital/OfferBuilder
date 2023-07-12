c553b6788a36d468b3143abdb70ae492018241c8
butvinm <butvin.mihail@yandex.ru>
2023-07-12 06:01:08 +0300

    Apply routes names as operations IDs
    See
    https://fastapi.tiangolo.com/ru/advanced/path-operation-advanced-configuration/

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@
 """
 
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 
 from app.api.routes.agents import router as agents_router
 from app.api.routes.auth import router as auth_router
@@ -12,6 +13,23 @@ from app.api.routes.offer_tpls import router as offer_tpls_router
 from app.api.routes.users import router as users_router
 from app.api.routes.wastes import router as wastes_router
 from app.api.routes.works import router as works_router
+
+
+def use_route_names_as_operation_ids(app: FastAPI) -> None:
+    """Simplify operation IDs.
+
+    Used for generated API clients to have simpler function names.
+
+    Should be called only after all routes have been added.
+
+    See https://fastapi.tiangolo.com/ru/advanced/path-operation-advanced-configuration/ for more details. # noqa: E501
+
+    Args:
+        app (FastAPI): FastAPI application.
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name  # in this case, 'read_items'
 
 
 def create_app() -> FastAPI:
@@ -33,3 +51,4 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+use_route_names_as_operation_ids(app)
