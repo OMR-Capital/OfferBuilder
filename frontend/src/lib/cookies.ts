edb9077745594310961de4e5e80b826b92cdc0b4
butvinm <butvin.mihail@yandex.ru>
2023-07-18 16:47:24 +0300

    Use minutes as cookies expiring units

--- frontend/src/lib/cookies.ts
+++ frontend/src/lib/cookies.ts
@@ -3,13 +3,13 @@ import { browser } from '$app/environment';
 /**
  * Set a cookie with a given name and value and expiration days.
  */
-export function setCookie(name: string, value: any, expirationHours: number) {
+export function setCookie(name: string, value: any, expirationMinutes: number) {
 	if (!browser) {
 		return false;
     }
 
 	const date = new Date();
-	date.setTime(date.getTime() + expirationHours * 24 * 60 * 60 * 1000);
+	date.setTime(date.getTime() + expirationMinutes * 60 * 1000);
 	const expires = 'expires=' + date.toUTCString();
 	document.cookie = name + '=' + value + ';' + expires + ';path=/';
 	return true;
