ab5b59fcdecc0343b3c8e149d8af2345eeda1fb6
butvinm <butvin.mihail@yandex.ru>
2023-07-20 07:33:59 +0300

    Implement CircularLoader and IconButton

---
+++ frontend/src/lib/components/CircularLoader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import CircularProgress from '@smui/circular-progress';
+
+    type Size = "small" | "medium" | "large";
+    export let size: Size = "medium";
+
+    function getStyle() {
+        switch (size) {
+            case "small":
+                return "width: 24px; height: 24px;";
+            case "medium":
+                return "width: 36px; height: 36px;";
+            case "large":
+                return "width: 48px; height: 48px;";
+        }
+    }
+</script>
+
+<CircularProgress {...$$restProps} style={getStyle()} indeterminate />
---
+++ frontend/src/lib/components/IconButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import IconButton from '@smui/icon-button';
+
+	export let icon: string;
+    export let onClick: () => void;
+</script>
+
+<IconButton {...$$restProps} on:click={onClick} class="material-icons">{icon}</IconButton>
