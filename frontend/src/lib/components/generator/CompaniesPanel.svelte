ce34c98ffcb8072bedaedb2c718302fff06585d3
butvinm <butvin.mihail@yandex.ru>
2023-07-23 10:04:32 +0300

    Impl generator organization selection panel

---
+++ frontend/src/lib/components/generator/CompaniesPanel.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { CompaniesAPI } from '$lib/backend/api/companies';
+	import type { Company } from '$lib/backend/models/companies';
+	import Autocomplete from '@smui-extra/autocomplete';
+	import { onMount } from 'svelte';
+	import CircularLoader from '../common/CircularLoader.svelte';
+	import Panel from '../common/Panel.svelte';
+	import Snackbar from '../common/Snackbar.svelte';
+
+	export let token: string;
+
+	let snackbar: Snackbar;
+
+	const companiesApi = new CompaniesAPI(token);
+
+	export let selectedCompany: Company | undefined = undefined;
+	let companies: Company[] = [];
+	let companiesLoading = false;
+
+	async function updateCompanies() {
+		companiesLoading = true;
+		const result = await companiesApi.getCompanies();
+		if (result.ok) {
+			companies = result.value;
+		} else {
+			snackbar.show(result.error.message);
+		}
+		companiesLoading = false;
+	}
+
+	onMount(updateCompanies);
+</script>
+
+<Panel title="Организация">
+	{#if companiesLoading}
+		<CircularLoader size="large" />
+	{:else}
+        <Autocomplete
+            label="Организация"
+            combobox
+            options={companies}
+            getOptionLabel={(option) => (option ? `${option.name}` : '')}
+            bind:value={selectedCompany}
+            textfield$style="width: 100%;"
+        />
+	{/if}
+</Panel>
+
+<Snackbar bind:this={snackbar} />
---
+++ frontend/src/routes/(authorized)/generator/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Company } from '$lib/backend/models/companies.js';
+	import CompaniesPanel from '$lib/components/generator/CompaniesPanel.svelte';
+
+	export let data;
+
+	let company: Company | undefined = undefined;
+</script>
+
+<!--
+<h1>Generator</h1>
+<p>template</p>
+<p>organization</p>
+<p>contr-agent</p>
+<p>type</p>
+<p>wastes and works table</p>
+<p>add item</p>
+<p>generate</p> -->
+
+<div class="panels-container">
+	<CompaniesPanel token={data.token} bind:selectedCompany={company} />
+</div>
