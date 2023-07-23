1513a830e7aebf2d8e70ca1c2d593538e3e0cc84
butvinm <butvin.mihail@yandex.ru>
2023-07-23 12:44:59 +0300

    Impl generator template selection panel

---
+++ frontend/src/lib/components/generator/OfferTplsPanel.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import { OfferTplsAPI } from '$lib/backend/api/offer_tpls';
+	import type { OfferTpl } from '$lib/backend/models/offer_tpls';
+	import Button, { Icon } from '@smui/button';
+	import { Label } from '@smui/common';
+	import Select, { Option } from '@smui/select';
+	import { onMount } from 'svelte';
+	import CircularLoader from '../common/CircularLoader.svelte';
+	import Panel from '../common/Panel.svelte';
+	import Snackbar from '../common/Snackbar.svelte';
+
+	export let token: string;
+
+	type Fruit = { id: number; label: string; price: number };
+	let fruits: Fruit[] = [
+		{ id: 1, label: 'Apple', price: 35 },
+		{ id: 2, label: 'Orange', price: 38 },
+		{ id: 3, label: 'Banana', price: 28 },
+		{ id: 4, label: 'Mango', price: 25 }
+	];
+
+	let valueA: Fruit | null = null;
+
+	let snackbar: Snackbar;
+
+	const offerTplsApi = new OfferTplsAPI(token);
+
+	export let selectedOfferTpl: OfferTpl | null = null;
+	let offerTpls: OfferTpl[] = [];
+	let offerTplsLoading = false;
+	let fileDownloading = false;
+
+	async function updateOfferTpls() {
+		offerTplsLoading = true;
+		const result = await offerTplsApi.getOfferTpls();
+		if (result.ok) {
+			offerTpls = result.value;
+		} else {
+			snackbar.show(result.error.message);
+		}
+		offerTplsLoading = false;
+	}
+
+	function getOfferTplKey(offerTpl: OfferTpl | null): string {
+		return offerTpl ? offerTpl.offer_tpl_id : '';
+	}
+
+	async function downloadOfferTpl() {
+		if (selectedOfferTpl == null) return;
+
+		fileDownloading = true;
+		const result = await offerTplsApi.downloadOfferTpl(selectedOfferTpl.offer_tpl_id);
+		fileDownloading = false;
+		if (result.ok) {
+			const blob = result.value;
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.href = url;
+			a.download = selectedOfferTpl.name + '.docx';
+			document.body.appendChild(a);
+			a.click();
+			a.remove();
+		} else {
+			snackbar.show(result.error.message);
+		}
+	}
+
+	onMount(updateOfferTpls);
+</script>
+
+<Panel title="Шаблон документа">
+	{#if offerTplsLoading}
+		<CircularLoader size="large" />
+	{:else}
+		<div class="offerTpl-selection-container">
+			<Select label="Шаблон" key={getOfferTplKey} bind:value={selectedOfferTpl}>
+				{#each offerTpls as offerTpl (getOfferTplKey(offerTpl))}
+					<Option value={offerTpl}>{offerTpl.name}</Option>
+				{/each}
+			</Select>
+
+			<div class="download-btn-container">
+				{#if fileDownloading}
+					<CircularLoader size="small" />
+				{:else}
+					<Button
+						variant="outlined"
+						disabled={selectedOfferTpl == null}
+						on:click={downloadOfferTpl}
+					>
+						<Icon class="material-icons">download_outlined</Icon>
+						<Label>Скачать выбранный шаблон</Label>
+					</Button>
+				{/if}
+			</div>
+		</div>
+	{/if}
+</Panel>
+
+<Snackbar bind:this={snackbar} />
+
+<style>
+	.offerTpl-selection-container {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+    .download-btn-container {
+        display: flex;
+        justify-content: flex-start;
+    }
+</style>
--- frontend/src/routes/(authorized)/generator/+page.svelte
+++ frontend/src/routes/(authorized)/generator/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import type { Company } from '$lib/backend/models/companies.js';
+	import type { Company } from '$lib/backend/models/companies';
+	import type { OfferTpl } from '$lib/backend/models/offer_tpls';
 	import CompaniesPanel from '$lib/components/generator/CompaniesPanel.svelte';
+	import OfferTplsPanel from '$lib/components/generator/OfferTplsPanel.svelte';
 
 	export let data;
 
-	let company: Company | undefined = undefined;
+	let offerTpl: OfferTpl | null = null;
+	let companyName: string | undefined = undefined;
 </script>
 
 <!--
 <h1>Generator</h1>
-<p>template</p>
-<p>organization</p>
++ <p>template</p>
++ <p>organization</p>
 <p>contr-agent</p>
 <p>type</p>
 <p>wastes and works table</p>
@@ -18,5 +21,15 @@
 <p>generate</p> -->
 
 <div class="panels-container">
-	<CompaniesPanel token={data.token} bind:selectedCompany={company} />
+	<OfferTplsPanel token={data.token} bind:selectedOfferTpl={offerTpl} />
+	<CompaniesPanel token={data.token} bind:selectedCompanyName={companyName} />
 </div>
+
+<style>
+    .panels-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        gap: 4rem;
+    }
+</style>
