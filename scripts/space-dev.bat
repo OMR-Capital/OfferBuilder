8da5170709299290afa8815242a502765c8d6d1b
butvinm <butvin.mihail@yandex.ru>
2023-07-30 07:01:15 +0300

    Add PDF_API_KEY env var
    PDF_API_KEY is API token for PSPDFKit utilized for docx converting to
    pdf.
    
    See https://pspdfkit.com/ for API details

--- scripts/space-dev.bat
+++ scripts/space-dev.bat
@@ -3,5 +3,6 @@ poetry export -f requirements.txt -o requirements.txt --without-hashes
 SET JWT_SECRET_KEY=09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7
 SET ACCESS_TOKEN_EXPIRE_MINUTES=30
 SET AGENTS_API_KEY=2f5f9952ea5002bb29eec5951579a85c042b86cd
+SET PDF_API_KEY=pdf_live_ZAXDYI8YD7kvqWVXoofhoeldvNwF5IL2zQW9xQIr4fm
 cd ..
 space dev
