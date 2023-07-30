c96d13ea8f50438eb4cb9c113ef27499ac0e563f
butvinm <butvin.mihail@yandex.ru>
2023-07-30 23:28:23 +0300

    Make Secutiry-Content-Policy adaptive
    Enable unsecure upgrade only for production environtment.
    
    It is required to fix Deta bug with proxy and allow to make secure
    requests during local development.

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	{#if !$page.url.host.includes('localhost')}
+		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+	{/if}
+</svelte:head>
+
+<slot />
